d2444ee5973dd714ec5b355489db910d4aabfdbb
LyunJ <tedlee0118@gmail.com>
2021-10-22 11:10:37 +0900

    유레카코드 주석 처리

--- modelService/modelService/__init__.py
+++ modelService/modelService/__init__.py
@@ -1,6 +1,6 @@
-import py_eureka_client.eureka_client as eureka
+# import py_eureka_client.eureka_client as eureka
 
-eureka.init(eureka_server="http://203.252.166.207:8761",
-                                app_name="djangoApp",
-                                instance_ip="121.130.68.170",
-                                instance_port=8000)
+# eureka.init(eureka_server="http://203.252.166.207:8761",
+#                                 app_name="djangoApp",
+#                                 instance_ip="121.130.68.170",
+#                                 instance_port=8000)
