89d1f2d82ef6b92deef2f6eb10c64f5b69be1adb
JasonYoo1995 <jasonyoo950909@gmail.com>
2021-10-26 00:36:41 +0900

    add load test program

---
+++ load_test/load_test.js
@@ -0,0 +1,24 @@
+const XMLHttpRequest = require("xhr2");
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("../ips/ips.json");
+let student = JSON.parse(rawdata);
+let ip = student["kafka"];
+
+let url = "http://" + ip + ":18002/labeling/test";
+
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  var xmlHttp = new XMLHttpRequest();
+  for (let i = 1; i <= 100000; i++) {
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.send(null);
+    console.log(i);
+    await timeout(100);
+  }
+}
+
+main();
